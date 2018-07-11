d86c5ecec27bfaa61d8f34db76ce4d43f3efda66
li.sun <li.sun@bitmain.com>
2018-07-11 17:25:44 +0800

    update cartoon_train.py

--- cartoon_train.py
+++ cartoon_train.py
@@ -13,6 +13,8 @@ if __name__ == '__main__':
     dataset_size = len(data_loader)
     print('#training images = %d' % dataset_size)
 
+    return ""
+
     model = create_model(opt)
     #visualizer = Visualizer(opt)
     for epoch in range(10):
@@ -46,34 +48,8 @@ if __name__ == '__main__':
 
         model.save('latest')
         model.save(epoch)
-            #    if total_steps % opt.display_freq == 0:
-            #    save_result = total_steps % opt.update_html_freq == 0
-                #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
 
-            #if total_steps % opt.print_freq == 0:
-               # errors = model.get_current_errors()
-                #t = (time.time() - iter_start_time) / opt.batchSize
-                #visualizer.print_current_errors(epoch, epoch_iter, errors, t, t_data)
-                #if opt.display_id > 0:
-                   # visualizer.plot_current_errors(epoch, float(epoch_iter) / dataset_size, opt, errors)
-
-            #if total_steps % opt.save_latest_freq == 0:
-            #    print('saving the latest model (epoch %d, total_steps %d)' %
-            #          (epoch, total_steps))
-            #model.save('latest')
-
-            #iter_data_time = time.time()
-
-        #print loss
-        #if epoch % opt.save_epoch_freq == 0:
-        #    print('saving the model at the end of epoch %d, iters %d' %
-        #          (epoch, total_steps))
-        #    model.save('latest')
-
-        #print('End of epoch %d / %d \t Time Taken: %d sec' %
-        #     (epoch, opt.niter + opt.niter_decay, time.time() - epoch_start_time))
-    #model.update_learning_rate()
-    #total_steps = 0
+    #train cartoon_net
     for epoch in range(opt.epoch_count, opt.niter + opt.niter_decay + 1):
         epoch_start_time = time.time()
         iter_data_time = time.time()
@@ -102,28 +78,7 @@ if __name__ == '__main__':
             loss[1] += errors["G"].data.cpu().numpy()[0]
             loss[2] += errors["D"].data.cpu().numpy()[0]
 
-            #if total_steps % opt.display_freq == 0:
-            #   save_result = total_steps % opt.update_html_freq == 0
-            #visualizer.display_current_results(model.get_current_visuals(), epoch, save_result)
-
-            #if total_steps % opt.print_freq == 0:
-            #   errors = model.get_current_errors()
-            #    t = (time.time() - iter_start_time) / opt.batchSize
-                #visualizer.print_current_errors(epoch, epoch_iter, errors, t, t_data)
-                #if opt.display_id > 0:
-                   # visualizer.plot_current_errors(epoch, float(epoch_iter) / dataset_size, opt, errors)
-
-            #if total_steps % opt.save_latest_freq == 0:
-            #   print('saving the latest model (epoch %d, total_steps %d)' %
-            #          (epoch, total_steps))
-            #   model.save('latest')
-
-            # iter_data_time = time.time()
-
-        #print loss
-        #if epoch % opt.save_epoch_freq == 0:
-        print('saving the model at the end of epoch %d, iters %d' %
-        #          (epoch, total_steps))
+        print('saving the model at the end of epoch %d, iters %d' %epoch)
         model.save('latest')
         model.save(epoch)
 
